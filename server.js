06734c9ec4ed0a8df5a1e30227bb6bf28260107a
DerGreed <joel.hilberg@gmail.com>
2025-08-27 13:40:48 +0200

    added dark mode css (2 ways to use)

--- server.js
+++ server.js
@@ -4,32 +4,71 @@ import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
+const USE_BODY_CLASS = false;
 
 // Middleware
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+function darkmode (theme) {
+  let sheetname;
+  switch (theme) {
+    case 'dark':
+      sheetname = 'darkmode';
+      break;
+    default:
+      return '';
+  }
+
+  return `
+    <head>
+      <link type="text/css" rel="stylesheet" href="/styles/${sheetname}.css">
+    </head>
+  `;
+}
+
+const HEAD = `
+  <head>
+    <link type="text/css" rel="stylesheet" href="/styles/style.css">
+  </head>
+`;
+
+const body = (theme, innerHtml) => `
+  <body${theme == 'dark' && ' class="darkmode"'}>
+    ${innerHtml}
+  </body>
+`;
+
+const addTheme = (theme, html) =>
+  USE_BODY_CLASS ?
+  HEAD + body(theme, html) :
+  darkmode(theme) + html
+;
 
 // Startseite
 app.get("/", (req, res) => {
   const username = req.cookies.username;
   const theme = req.cookies.theme || 'light';
 
+  let html;
   if (username) {
-    res.send(`
+    html = `
       <h1>Willkommen zurück, ${username}!</h1>
       <p>Aktuelles Theme: ${theme}</p>
       <a href="/settings">Einstellungen</a> |
       <a href="/logout">Logout</a>
-    `);
+    `;
   } else {
-    res.send(`
+    html = `
       <h1>Hallo Gast!</h1>
       <form method="POST" action="/login">
         <input type="text" name="username" placeholder="Benutzername" required />
         <button type="submit">Login</button>
       </form>
-    `);
+    `;
   }
+  res.send(addTheme(theme, html));
 });
 
 // Login → Cookie setzen
@@ -46,7 +85,7 @@ app.post("/login", (req, res) => {
 // Einstellungen
 app.get("/settings", (req, res) => {
   const theme = req.cookies.theme || 'light';
-  res.send(`
+  const html = `
     <h1>Einstellungen</h1>
     <form method="POST" action="/theme">
       <label>Theme auswählen:</label>
@@ -57,7 +96,8 @@ app.get("/settings", (req, res) => {
       <button type="submit">Speichern</button>
     </form>
     <a href="/">Zurück</a>
-  `);
+  `;
+  res.send(addTheme(theme, html));
 });
 
 // Theme ändern
@@ -74,6 +114,12 @@ app.get("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/");
 });
+
+// app.get('/style.css', function(req, res) {
+//   res.sendFile("./style.css");
+// });
+
+app.use('/styles', express.static('styles'))
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
