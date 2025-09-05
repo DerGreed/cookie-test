b9e18109bf8fc7dec36f00c452ebd6c9c9875ed9
DerGreed <joel.hilberg@gmail.com>
2025-09-05 16:32:56 +0200

    changed css selector

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
-const USE_BODY_CLASS = false;
+const USE_BODY_CLASS = true;
 
 // Middleware
 app.use(cookieParser());
